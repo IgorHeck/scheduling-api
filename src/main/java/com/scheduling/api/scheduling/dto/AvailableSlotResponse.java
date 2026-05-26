0aec7b4198688e6714d57a822a084eb6b3ecace8
IgorHeck <igor.heck2002@gmail.com>
2026-05-25 21:46:07 -0300

    fix: ajuste sobre endpoints

--- src/main/java/com/scheduling/api/scheduling/dto/AvailableSlotResponse.java
+++ src/main/java/com/scheduling/api/scheduling/dto/AvailableSlotResponse.java
@@ -1,12 +1,16 @@
 package com.scheduling.api.scheduling.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AvailableSlotResponse {
 
     private LocalDateTime startAt;
